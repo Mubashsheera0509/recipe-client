28974d50536a292503820497301ee565bd01c49a
Mubashsheera0509 <mubashsheera555@gmail.com>
2025-03-02 16:05:02 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AppContext } from "../context/App_Context";
 
 const Navbar = () => {
-  return (
-    <>
-    
-    <div className="navbar-container">
-    <h2 className="logo">SAVORISM</h2>
-    <div className="nav-buttons">
-      <Link to={"/login"} className="btn btn-primary mx-2">Login</Link>
-      <Link to={"/register"}className="btn btn-warning mx-2">Register</Link>
-      <Link to={"/add"} className="btn btn-info mx-2">Add</Link>
-      <Link to={"/profile"} className="btn btn-warning mx-2">Profile</Link>
-      <button className="btn btn-danger mx-2">Logout</button>
-      <Link to={"/saved"}className="btn btn-light mx-2">Saved</Link>
-      </div>
-  
-      </div>
+  const { isAuthenticated, logOut } = useContext(AppContext);
 
-    </>
+  return (
+    <div className="navbar-container bg-dark p-2 d-flex justify-content-between align-items-center">
+      <Link to="/" className="text-white text-decoration-none">
+        <h2>SAVORISM</h2>
+      </Link>
+      <div className="nav-buttons d-flex align-items-center">
+        {isAuthenticated ? (
+          <>
+            <Link to="/add" className="btn btn-info mx-2">
+              Add
+            </Link>
+            <Link to="/profile" className="btn btn-warning mx-2">
+              Profile
+            </Link>
+            <button className="btn btn-danger mx-2" onClick={logOut}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <Link to="/login" className="btn btn-primary mx-2">
+              Login
+            </Link>
+            <Link to="/register" className="btn btn-warning mx-2">
+              Register
+            </Link>
+          </>
+        )}
+        <Link to="/saved" className="btn btn-light mx-2">
+          Saved
+        </Link>
+      </div>
+    </div>
   );
 };
 
